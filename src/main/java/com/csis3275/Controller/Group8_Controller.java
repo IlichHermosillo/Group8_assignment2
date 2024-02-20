6fe2b7ea1c2cd182b026c7b2af82ef70154a3c49
Ilich Hermosillo <hermosillohernandezi@student.douglascollege.ca>
2024-02-19 16:48:33 -0800

    ihe-49View added

--- src/main/java/com/csis3275/Controller/Group8_Controller.java
+++ src/main/java/com/csis3275/Controller/Group8_Controller.java
@@ -1,15 +1,23 @@
 package com.csis3275.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.csis3275.Model.Group8_Model;
 
 @Controller
 public class Group8_Controller {
 
 	@GetMapping("/")
-	public String displayMain() {
+	public String displayIdex() {
 		return("index.html");
 	}
 	
-	
+	@GetMapping("/group8-ilich")
+	public String displayStudents(Model model) {
+		Group8_Model st = new Group8_Model(300358049, 3275, 002, "Ilich", "Hermosillo", "CSIS - Data Analyst");
+		model.addAttribute("st", st);
+		return "ihe-49-View";
+	}
 }
