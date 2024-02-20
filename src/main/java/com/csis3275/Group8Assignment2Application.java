b426312b60b67974af7e6daa37ada20a6352685b
Ilich Hermosillo <hermosillohernandezi@student.douglascollege.ca>
2024-02-19 16:19:55 -0800

    Master Branch First commit

---
+++ src/main/java/com/csis3275/Group8Assignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group8Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group8Assignment2Application.class, args);
+	}
+
+}
